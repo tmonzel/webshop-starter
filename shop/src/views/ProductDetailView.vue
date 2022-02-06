9c400b16704f292247b1e347c331f9e00a86e292
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-06 16:05:35 +0100

    Add cart button

--- shop/src/views/ProductDetailView.vue
+++ shop/src/views/ProductDetailView.vue
@@ -2,12 +2,16 @@
   <div class="container mt-5">
     <h1>{{ product?.name }}</h1>
     <p class="lead">{{ product?.type }}</p>
+    <div>
+      <button class="btn btn-primary" @click="addToCart">In den Warenkorb</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Product } from '@/core';
 import { productService } from '@/data';
+import { useCart } from '@/features/cart';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
@@ -16,13 +20,19 @@ export default defineComponent({
 
   setup(props) {
     const product = ref<Product>();
+    const { addItem } = useCart();
 
     productService.findOne(props.id).subscribe(p =>{
       product.value = p;
     });
 
+    const addToCart = () => {
+      addItem({ product: product.value as Product, config: {}, _id: '' });
+    }
+
     return {
       product,
+      addToCart
     }
   }
 });
