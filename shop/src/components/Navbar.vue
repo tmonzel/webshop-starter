5b5368f6eb61f03ac8d12e0180001fe726a59a81
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-06 16:01:50 +0100

    Adjust navbar styles and links

--- shop/src/components/Navbar.vue
+++ shop/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light shadow-lg">
+  <nav class="navbar navbar-expand-md navbar-light bg-light shadow-lg">
     <div class="container">
       <RouterLink class="navbar-brand" to="/">
         <i class="bi-shop" style="font-size: 2rem"></i>
@@ -23,7 +23,10 @@
             <RouterLink class="nav-link" to="/products">Produkte</RouterLink>
           </li>
           <li class="nav-item">
-            <RouterLink class="nav-link" to="/cart">Warenkorb <span class="badge bg-primary">2</span></RouterLink>
+            <RouterLink class="nav-link" to="/orders">Bestellungen</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/cart">Warenkorb <span class="badge bg-primary">{{ cart.items.length }}</span></RouterLink>
           </li>
         </ul>
       </div>
@@ -33,12 +36,18 @@
 </template>
 
 <script lang="ts">
+import { useCart } from '@/features/cart';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Navbar',
 
   setup() {
+    const { state } = useCart();
+
+    return {
+      cart: state
+    }
   }
 });
 </script>
