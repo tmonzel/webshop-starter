d9d9d574a83940cd065ccb8fa783959461814189
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-07 21:08:46 +0100

    Optimize cart view

--- shop/src/views/CartView.vue
+++ shop/src/views/CartView.vue
@@ -8,24 +8,33 @@
             <span>
               {{ item.product.name }} ({{ item.product.type }})
             </span>
-            <span>
-              <button class="btn p-0 shadow-none" @click="removeItem(index)">
-                <i class="bi-trash"></i>
-              </button>
-            </span>
+            <div>
+              <span class="me-3">{{ item.product.price.value }} {{ item.product.price.currency }}</span>
+              <span>
+                <button class="btn p-0 shadow-none" @click="removeItem(index)">
+                  <i class="bi-trash"></i>
+                </button>
+              </span>
+            </div>
           </li>
         </ul>
         <div v-else class="alert alert-info">
           Der Warenkorb ist leer.
         </div>
       </div>
-      <div class="col-md-3 bg-light p-4">
+      <div class="col-md-3">
+        <div class="bg-light p-4 mb-3">
+          <div class="fs-4 text-end mb-3">
+            <span class="lead">Gesamt</span><br>{{ priceTotal.value }} {{ priceTotal.currency }}
+          </div>
+        </div>
+
         <div class="d-flex justify-content-end" v-if="auth.user">
           <button class="btn btn-lg btn-primary" @click="orderCart">Bestellen</button>
         </div>
 
-        <div class="alert alert-info" v-else>
-          Bitte einloggen um zu bestellen.
+        <div class="alert alert-warning" v-else>
+          Bitte <router-link to="/login">einloggen</router-link> um zu bestellen.
         </div>
       </div>
     </div>
@@ -43,14 +52,15 @@ export default defineComponent({
   name: 'CartView',
 
   setup() {
-    const { state: cart, removeItem, orderCart } = useCart();
+    const { state: cart, removeItem, orderCart, priceTotal } = useCart();
     const { state: auth } = useAuth();
 
     return {
       cart,
       auth,
       orderCart,
-      removeItem
+      removeItem,
+      priceTotal
     }
   }
 });
