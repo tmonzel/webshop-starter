1f258044be88d498f2be3c1f3ca9ba812eb926ec
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-07 21:05:51 +0100

    Type does not need to inherit from Document

--- api/src/resource-creator.ts
+++ api/src/resource-creator.ts
@@ -2,7 +2,7 @@ import { Express } from 'express';
 import { ObjectId } from 'mongodb';
 import { Document, Model } from 'mongoose';
 
-export const createResource = <T extends Document>(api: Express, model: Model<T>) => {
+export const createResource = <T>(api: Express, model: Model<T>) => {
     api.get("/" + model.collection.name, (req, res) => {
         model.find().then(result => {
             res.json(result);
