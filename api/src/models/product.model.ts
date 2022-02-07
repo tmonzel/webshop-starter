43403ad870d6ba62a9472eb6b0f095ed2af32b70
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-07 21:04:57 +0100

    Add quick product fixtures

--- api/src/models/product.model.ts
+++ api/src/models/product.model.ts
@@ -1,13 +1,19 @@
-import { model, Schema, Document } from 'mongoose';
+import { model, Schema } from 'mongoose';
 
-export interface Product extends Document {
+export interface Product {
     name: string;
     type: string;
+    description: string;
+    imageUrl: string;
+    price: { value: number; currency: string };
 }
 
 const schema = new Schema<Product>({
     name: { type: String, required: true },
-    type: { type: String }
+    type: String,
+    description: String,
+    imageUrl: String,
+    price: { value: Number, currency: String }
 })
 
 export const ProductModel = model<Product>('Product', schema);
