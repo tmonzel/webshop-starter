fefc0d79dee57eeb2e0c3b1c3c1590143a6d17ed
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-08 10:36:09 +0100

    Show remote errors

--- shop/src/views/LoginView.vue
+++ shop/src/views/LoginView.vue
@@ -1,42 +1,50 @@
 <template>
-  <div class="container mt-5">
-    <h1 class="mb-5">Anmelden</h1>
-    <form 
-      @submit.prevent="onSubmit" 
-      :class="{ 'was-validated': showValidations && !form.errors.email }" 
-      style="max-width: 320px" 
-      novalidate
-    >
-      <div class="mb-3">
-        <input 
-          type="email" 
-          class="form-control form-control-lg" 
-          placeholder="E-Mail"
-          v-model="form.data.email"
-          :class="{ 'is-invalid': form.errors.email }"
-          required
-        >
-        <div class="invalid-feedback" v-if="form.errors.email">
-          {{ form.errors.email.message }}
+  <div class="container mt-5 d-flex justify-content-center">
+    <div>
+      <h1 class="mb-5">Anmelden</h1>
+      <form 
+        @submit.prevent="onSubmit" 
+        :class="{ 'was-validated': showValidations && !form.errors?.email }" 
+        novalidate
+      >
+        <div class="mb-3">
+          <input 
+            type="email" 
+            class="form-control form-control-lg" 
+            placeholder="E-Mail"
+            v-model="form.data.email"
+            :class="{ 'is-invalid': form.errors?.email }"
+            required
+          >
+          <div class="invalid-feedback" v-if="form.errors?.email">
+            {{ form.errors.email.message }}
+          </div>
+          <div class="invalid-feedback" v-else>
+            Sie müssen eine korrekte E-Mail eingeben
+          </div>
         </div>
-        <div class="invalid-feedback" v-else>
-          Sie müssen eine korrekte E-Mail eingeben
+        <div class="mb-3">
+          <input 
+            type="password" 
+            class="form-control form-control-lg" 
+            placeholder="Password"
+            v-model="form.data.password"
+            required
+          >
+          <div class="invalid-feedback">
+            Sie müssen ein Passwort eingeben
+          </div>
         </div>
-      </div>
-      <div class="mb-3">
-        <input 
-          type="password" 
-          class="form-control form-control-lg" 
-          placeholder="Password"
-          v-model="form.data.password"
-          required
-        >
-        <div class="invalid-feedback">
-          Sie müssen ein Passwort eingeben
+        <div class="alert alert-danger" v-if="hasErrors">
+          Anmeldung fehlgeschlagen.<br>
+          Überprüfen Sie Ihre Zugangsdaten
         </div>
-      </div>
-      <button type="submit" class="btn btn-lg btn-primary">Anmelden</button>
-    </form>
+        <div class="mb-3">
+          <small>Noch kein Kunde? Registrieren Sie sich jetzt <router-link to="/signup">hier</router-link>.</small>
+        </div>
+        <button type="submit" class="btn btn-lg btn-primary">Anmelden</button>
+      </form>
+    </div>
   </div>
 
   
@@ -75,11 +83,12 @@ export default defineComponent({
   },
 
   setup() {
-    const { form, submit } = useLoginForm();
+    const { form, submit, hasErrors } = useLoginForm();
 
     return {
       form,
       submit,
+      hasErrors
     }
   },
 });
