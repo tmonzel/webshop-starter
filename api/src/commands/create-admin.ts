fe2927c1f7642d7ff605fd88aff7a50dc2226d61
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-13 09:22:09 +0100

    Add pseudo/testing admin create command

---
+++ api/src/commands/create-admin.ts
@@ -0,0 +1,21 @@
+import { establishConnection } from '../database';
+import { UserModel, UserRoles } from '../models/user.model';
+import bcrypt from 'bcrypt';
+
+establishConnection(() => {
+    const user = new UserModel({
+        email: 'admin@root.de',
+        username: 'admin',
+        password: bcrypt.hashSync('root', 8),
+        roles: [UserRoles.ADMIN]
+    });
+
+    user.save(err => {
+        if(err) {
+            console.error('Could not create admin user');
+            return;
+        }
+
+        console.log('Admin user created');
+    });
+});
