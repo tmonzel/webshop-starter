6e669fa2690a5eeb123e112918cafc1ba6525873
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-16 08:19:55 +0100

    Add admin orders table

--- web/apps/admin/views/OrdersView.vue
+++ web/apps/admin/views/OrdersView.vue
@@ -1,14 +1,39 @@
 <template>
   <div>
     <h1>Bestellungen</h1>
+    <table class="table">
+      <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Kunde</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="p in state.items" :key="p._id">
+            <th scope="row">{{ p._id }}</th>
+            <td>{{ p.user?.username }}</td>
+          </tr>
+        </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
+import { useOrders } from '@/composables';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'OrdersView'
+  name: 'OrdersView',
+
+  setup() {
+    const { state, loadAll } = useOrders();
+
+    loadAll();
+
+    return {
+      state
+    }
+  }
 });
 </script>
 
