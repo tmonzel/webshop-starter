652ff66202cd56767bc4ebed08c851635df5f0b3
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-16 17:02:53 +0100

    Add createdAt timestamp to order schema

--- api/src/models/order.model.ts
+++ api/src/models/order.model.ts
@@ -4,23 +4,25 @@ import { User } from './user.model';
 export interface Order extends Document {
     items: any[];
     user: User;
+    createdAt: Date;
 }
 
-const ItemSchema = new Schema({ 
-    product: {
-        type: Schema.Types.ObjectId,
-        ref: 'Product'
-    },
-    quantity: Number,
-    config: Schema.Types.Mixed
-});
-
 const schema = new Schema<Order>({
-    items: [ItemSchema],
+    items: [{ 
+        product: {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        },
+        quantity: Number,
+        config: Schema.Types.Mixed
+    }],
+
     user: {
         type: Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+
+    createdAt: { type: Date, default: Date.now },
 })
 
 export const OrderModel: Model<Order> = model<Order>('Order', schema);
