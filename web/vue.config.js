d8658ea1d062e44df5cb4d507d52c49ad57cb86b
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-17 17:31:23 +0100

    Set custom webpack copy plugin public folders for each app

--- web/vue.config.js
+++ web/vue.config.js
@@ -4,20 +4,42 @@ const multiConfig = {
   shop: {
     pages: {
       index: {
-        entry: 'apps/shop/main.ts',
-        template: 'apps/shop/public/index.html',
+        entry: './apps/shop/main.ts',
+        template: './apps/shop/public/index.html',
         filename: 'index.html',
       }
+    },
+
+    chainWebpack(config) {
+      config
+        .plugin('copy')
+        .use(require('copy-webpack-plugin'))
+        .tap((args) => {
+          return [
+            [...(args[0] ? args[0] : []), { from: path.resolve('./apps/shop/public') }],
+          ]
+        });
     },
   },
 
   admin: {
     pages: {
       index: {
-        entry: 'apps/admin/main.ts',
-        template: 'apps/admin/public/index.html',
+        entry: './apps/admin/main.ts',
+        template: './apps/admin/public/index.html',
         filename: 'index.html',
       }
+    },
+
+    chainWebpack(config) {
+      config
+        .plugin('copy')
+        .use(require('copy-webpack-plugin'))
+        .tap((args) => {
+          return [
+            [...(args[0] ? args[0] : []), { from: path.resolve('./apps/admin/public') }],
+          ]
+        });
     },
   }
 }
