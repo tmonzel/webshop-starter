253710848dc7269d9b69829eb4d7252198dbfeee
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-21 08:29:45 +0100

    Add box icon to dashboard

--- web/apps/admin/views/DashboardView.vue
+++ web/apps/admin/views/DashboardView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <i class="bi bi-box-seam text-primary" style="font-size: 5rem;"></i>
     <h1 class="display-4 fw-bold mb-4" style="line-height: 1;">Lager<br>Verwaltung</h1>
     <p class="lead fs-4">Drücke ein paar Knöpfe hier und da,<br>aber nicht den Roten!</p>
   </div>
