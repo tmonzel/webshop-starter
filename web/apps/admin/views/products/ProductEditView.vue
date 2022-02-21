15540702e60803ad01ad02d65c18a1717e15e058
Thomas Monzel <t.monzel@gds-gmbh.de>
2022-02-21 08:28:32 +0100

    Redirect to /products on save

--- web/apps/admin/views/products/ProductEditView.vue
+++ web/apps/admin/views/products/ProductEditView.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
-    <div class="d-flex justify-content-between">
-      <h1 class="fw-bold mb-5">Produkt bearbeiten</h1>
-      <div>
-        <button 
-          class="btn btn-lg btn-success" 
-          @click="form?.submit" 
-          :disabled="state.loadingState === 'saving'"
-        ><i class="bi bi-save me-1"></i> Speichern</button>
-      </div>
+    <div class="d-flex mb-4">
+      <RouterLink
+        class="btn btn-lg btn-light me-2" 
+        to="/products"
+      ><i class="bi bi-arrow-left me-1"></i></RouterLink>
+      <button 
+        class="btn btn-lg btn-primary me-2" 
+        @click="form?.submit" 
+        :disabled="state.loadingState === 'saving'"
+      ><i class="bi bi-save me-1"></i> Speichern</button>
+      <!--<button 
+        class="btn btn-lg btn-danger" 
+      ><i class="bi bi-trash me-1"></i> Löschen</button>-->
     </div>
+
+    <h1 class="fw-bold mb-5">Produkt bearbeiten</h1>
+
     <div style="max-width: 800px;">
-      <ProductForm ref="form" v-if="state.loadedItem" :product="state.loadedItem" />
+      <ProductForm 
+        ref="form" 
+        v-if="state.loadedItem" 
+        :product="state.loadedItem" 
+        @save="onProductSaved"
+      />
     </div>
   </div>
 </template>
@@ -20,6 +32,7 @@
 import { defineComponent, ref } from 'vue';
 import { ProductForm } from '@admin/features/product';
 import { useProducts, ProductModel } from '@/composables/product';
+import { router } from '@admin/routing';
 
 export default defineComponent({
   name: 'ProductEditView',
@@ -35,9 +48,14 @@ export default defineComponent({
     const form = ref<InstanceType<typeof ProductForm> | null>(null);
     const { state } = useProducts();
 
+    const onProductSaved = () => {
+      router.push('/products');
+    }
+
     return {
       form,
       state,
+      onProductSaved
     }
   }
 });
